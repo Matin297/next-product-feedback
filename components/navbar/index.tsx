f4915cada95da7d1112454b97e837419b75a215a
Matin Mir <asmamir297@gmail.com>
2024-04-17 16:14:57 +0330

    replace navbar file with navbar directory

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,19 +1,22 @@
+import Profile from "./profile";
+import Box from "@mui/material/Box";
+import MUILink from "@mui/material/Link";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import MUILink from "@mui/material/Link";
-import Box from "@mui/material/Box";
 import AcUnitTwoToneIcon from "@mui/icons-material/AcUnitTwoTone";
-import { Paper } from "@mui/material";
 
 export default function Navbar() {
   return (
     <AppBar variant="outlined" sx={{ backgroundColor: "common.white" }}>
       <Toolbar>
         <Container>
-          <Box display="flex" justifyContent="space-between">
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
             <MUILink underline="none" href="/">
               <Box display="flex" gap={2} alignItems="center">
                 <AcUnitTwoToneIcon fontSize="large" />
@@ -22,7 +25,7 @@ export default function Navbar() {
                 </Typography>
               </Box>
             </MUILink>
-            <Button size="large">Login</Button>
+            <Profile />
           </Box>
         </Container>
       </Toolbar>
