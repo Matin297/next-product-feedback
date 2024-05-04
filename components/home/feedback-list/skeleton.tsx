4f42ee083bd04b7fbb18c32602a9249dd5b102a9
Matin Mir <asmamir297@gmail.com>
2024-05-04 15:22:55 +0330

    remove the title skeleton from homepage feedback list

--- components/home/feedback-list/skeleton.tsx
+++ components/home/feedback-list/skeleton.tsx
@@ -1,14 +1,23 @@
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import Stack from "@mui/material/Stack";
 import Skeleton from "@mui/material/Skeleton";
+import AddIcon from "@mui/icons-material/Add";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
 import CardContent from "@mui/material/CardContent";
 
 const DATA = [1, 2];
 
 export default function FeedbackListSkeleton() {
   return (
-    <Stack spacing={1} flexGrow={1}>
-      <Skeleton height={50} width="100px" />
+    <Stack flexGrow={1} spacing={2}>
+      <Box display="flex" alignItems="center">
+        <Typography variant="h6">Suggestions</Typography>
+        <IconButton color="secondary" href="/feedback/create">
+          <AddIcon />
+        </IconButton>
+      </Box>
       <Stack spacing={2}>
         {DATA.map((item) => (
           <Card elevation={3} key={item}>
